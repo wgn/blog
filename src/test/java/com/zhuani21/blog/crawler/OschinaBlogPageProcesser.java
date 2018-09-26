d23e4210d64643a939f33cb45e5d57bf1d16a1c2
Wugn <zhuani21@163.com>
2018-09-26 17:47:36 +0800

    爬虫入门

---
+++ src/test/java/com/zhuani21/blog/crawler/OschinaBlogPageProcesser.java
@@ -0,0 +1,34 @@
+package com.zhuani21.blog.crawler;
+
+import java.util.List;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class OschinaBlogPageProcesser implements PageProcessor {
+
+    private Site site = Site.me().setDomain("www.runoob.com");
+
+    @Override
+    public void process(Page page) {
+        List<String> links = page.getHtml().links().regex("http://www\\.runoob\\.com/git/\\S+").all();
+        page.addTargetRequests(links);
+        page.putField("title", page.getHtml().xpath("//div[@class='article-body']/div[@class='article-intro']/h1").toString());
+        page.putField("content", page.getHtml().$("div#content").toString());
+        page.putField("tags",page.getHtml().xpath("//div[@class='next-design-link']/a/text()").all());
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new OschinaBlogPageProcesser()).addUrl("http://www.runoob.com/git")
+             .addPipeline(new ConsolePipeline()).run();
+    }
+}
